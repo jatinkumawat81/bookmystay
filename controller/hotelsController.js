9497eb836c7fea124c0c190f7ffda418b031f642
jatin <jatinkumawat530@gmail.com>
2026-04-12 21:55:10 +0530

    changes

--- controller/hotelsController.js
+++ controller/hotelsController.js
@@ -1,9 +1,32 @@
 const Hotel = require('../models/hotel'); 
 exports.getAll = async (req, res)=>{
     try{
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+        const queryObj = {...req.query};
+        excludeFields.forEach(el => delete queryObj[el]);
         // const hotels = await Hotel.find();
         // const hotels = await Hotel.find().where('city').equals(req.query.city);
-        const hotels = await Hotel.find(res.query);
+        let filterQuery = getFinalFilterQuery(queryObj);
+        // const hotels = await Hotel.find(filterQuery);
+        let query = Hotel.find(filterQuery);
+
+        //sorting results
+        if(req.query.sort){
+            query = query.sort(req.query.sort.split(',').join(' '));
+        }else{
+            query = query.sort('cheapestPrice');
+        }
+
+        //feild limiting
+        if(req.query.fields){
+            query = query.select(req.query.fields.split(',').join(' '));
+        }else{
+            query = query.select('-__v');
+        }
+        
+        const hotels = await query;
+        // const hotels = await Hotel.find({city: 'Mumbai', cheapestPrice: {$lte: 10000},rating: {$gte: 3}});
+
         res.status(200).json(
             {
                 status: 'success',
@@ -94,4 +117,23 @@ exports.delete = async (req, res)=>{
             error: err.message
         });
     }
+}
+
+const getFinalFilterQuery = (queryObj) => {
+    const finalQueryObj = {};
+    for(let key in queryObj){
+        const value = queryObj[key];
+        const match = key.match(/^(.*)\[(gte|lte|gt|lt)\]$/);
+        if(match){
+            const fieldName = match[1];
+            const operator = `$${match[2]}`;
+            if(!finalQueryObj[fieldName]){
+                finalQueryObj[fieldName] = {};
+            }
+            finalQueryObj[fieldName][operator] = value;
+        }else{
+            finalQueryObj[key] = value;
+        }
+    }
+    return finalQueryObj;
 }
